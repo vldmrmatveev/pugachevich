b615e66f7795f6bb0b18d7cb3b81532303fdc8e1
matvl <matvl@completo.ru>
2020-12-19 20:39:56 +0300

    fixed

--- src/modules/main-page/main/script.js
+++ src/modules/main-page/main/script.js
@@ -3,7 +3,18 @@ $('.main-slider').slick({
 	arrows: true,
   slidesToShow: 1,
 	slidesToScroll: 1,
-	autoplay: true
+	autoplay: true,
+	centerMode: true,
+	variableWidth: true,
+	responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+				variableWidth: false,
+				centerMode: false
+      }
+    }
+  ]
 });
 
 $('.collapsed-btn').click(function(){
