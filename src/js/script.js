c9121cc799ab1af10a9819acd66d6efd95a16413
matvl <matvl@completo.ru>
2020-12-21 11:04:02 +0300

    fixed

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,4 @@
 //= ../modules/main-page/main/script.js
 //= ../modules/main-page/crew/script.js
 //= ../modules/portfolio-single/script.js
-//= ../modules/service-single/script.js
 //= ../modules/blog-single-carousel/script.js
--- src/modules/service-single/script.js
+++
@@ -1,7 +0,0 @@
-$('.service-slider').slick({
-	dots: false,
-	arrows: true,
-  slidesToShow: 1,
-	slidesToScroll: 1,
-	centerMode: true
-});
