c89633595b6352d5f672eb629393e9ab0a0c4c41
matvl <matvl@completo.ru>
2020-12-21 11:16:04 +0300

    fixed

--- src/modules/main-page/main/script.js
+++ src/modules/main-page/main/script.js
@@ -1,21 +1,23 @@
-$('.main-slider').slick({
-	dots: true,
-	arrows: true,
-  slidesToShow: 1,
-	slidesToScroll: 1,
-	autoplay: true,
-	centerMode: true,
-	variableWidth: true,
-	responsive: [
-    {
-      breakpoint: 1440,
-      settings: {
-				variableWidth: false,
-				centerMode: false
-      }
-    }
-  ]
-});
+window.onload = function() {
+	$('.main-slider').slick({
+		dots: true,
+		arrows: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		centerMode: true,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1440,
+				settings: {
+					variableWidth: false,
+					centerMode: false
+				}
+			}
+		]
+	});
+}
 
 $('.collapsed-btn').click(function(){
 	if ($(this).attr('aria-expanded') == 'true') {
